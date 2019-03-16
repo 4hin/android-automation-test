9b11c4d6748e0cd815640230003a216b8b30e015
s.kimura <s.kimura@creativehope.co.jp>
2019-03-16 20:38:48 +0900

    edit

--- app/src/main/java/com/example/test/automationtest/util/HogeUtil.java
+++ app/src/main/java/com/example/test/automationtest/util/HogeUtil.java
@@ -1,4 +1,11 @@
 package com.example.test.automationtest.util;
 
 public class HogeUtil {
+    public String hoge(int arg) {
+        String v;
+
+        if (arg == 0) {
+            return v;
+        }
+    }
 }
