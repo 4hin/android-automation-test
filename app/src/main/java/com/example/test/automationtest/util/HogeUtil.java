1f032140b923732c58fcec8c7ac50ecdfe71cf12
s.kimura <s.kimura@creativehope.co.jp>
2019-03-17 14:19:11 +0900

    edit

--- app/src/main/java/com/example/test/automationtest/util/HogeUtil.java
+++ app/src/main/java/com/example/test/automationtest/util/HogeUtil.java
@@ -1,6 +1,7 @@
 package com.example.test.automationtest.util;
 
 public class HogeUtil {
+
     public String hoge(int arg){
         String reutrnValeu="";
         int hoge=0;
@@ -9,5 +10,13 @@ public class HogeUtil {
             return reutrnValeu;
         }
         return "";
+    }
+
+    private int fuga(){
+        int v;
+        for(int i=0; i<10;i++){
+            v+=1;
+        }
+        return v;
     }
 }
